929c088ef7628ca99b2c1a7cd117469bb7b8108d
chenliangliang <liagnliangchen@maycur.com>
2018-09-23 12:15:52 +0800

    first commit

---
+++ src/main/java/me/chenll/zookeeperclientdemo/ZookeeperClientDemoApplication.java
@@ -0,0 +1,12 @@
+package me.chenll.zookeeperclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZookeeperClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZookeeperClientDemoApplication.class, args);
+	}
+}
